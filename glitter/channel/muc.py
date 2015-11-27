e5aeb0b7d6ba69aee3ac1bb7de1e413c8295675a
Diane Trout <diane@ghic.org>
2015-11-26 18:50:24 -0800

    update muc interface to match features of gitter

--- glitter/channel/muc.py
+++ glitter/channel/muc.py
@@ -34,17 +34,15 @@ class GlitterMucChannel(
         GlitterTextChannel,
         telepathy.server.ChannelInterfaceGroup):
 
-    def __init__(self, conn, manager, conversation, props, object_path=None):
-        GlitterTextChannel.__init__(self, conn, manager, conversation, props, object_path)
+    def __init__(self, conn, manager, room, props, object_path=None):
+        GlitterTextChannel.__init__(self, conn, manager, room, props, object_path)
         telepathy.server.ChannelInterfaceGroup.__init__(self)
 
-        # We would only ever be given a conversation on being invited to an
-        # existing MUC.
-        if conversation:
-            self._conversation = conversation
-            #papyon.event.ConversationEventInterface.__init__(self, self._conversation)
-
-        self.GroupFlagsChanged(telepathy.CHANNEL_GROUP_FLAG_CAN_ADD, 0)
+        self.GroupFlagsChanged(
+            telepathy.CHANNEL_GROUP_FLAG_CHANNEL_SPECIFIC_HANDLES |
+            telepathy.CHANNEL_GROUP_FLAG_HANDLE_OWNERS_NOT_AVAILABLE,
+            0
+        )
 
         # This is done in an idle so that classes which subclass this one
         # can do stuff in their __init__ but will still benefit from this method
@@ -58,36 +56,35 @@ class GlitterMucChannel(
         else:
             raise telepathy.PermissionDenied()
 
-    # papyon.event.ConversationEventInterface
-    def on_conversation_user_joined(self, contact):
+    def on_room_user_joined(self, contact):
         handle = self._conn.ensure_contact_handle(contact)
         logger.info("User %s joined" % str(handle))
 
         if handle not in self._members:
-            self.MembersChanged('', [handle], [], [], [],
-                    handle, telepathy.CHANNEL_GROUP_CHANGE_REASON_INVITED)
+            self.MembersChanged(
+                '', [handle], [], [], [],
+                handle, telepathy.CHANNEL_GROUP_CHANGE_REASON_INVITED)
 
-    # papyon.event.ConversationEventInterface
-    def on_conversation_user_left(self, contact):
+    def on_room_user_left(self, contact):
         handle = self._conn.ensure_contact_handle(contact)
         logger.info("User %s left" % str(handle))
 
-        self.MembersChanged('', [], [handle], [], [],
-                handle, telepathy.CHANNEL_GROUP_CHANGE_REASON_NONE)
+        self.MembersChanged(
+            '', [], [handle], [], [],
+            handle, telepathy.CHANNEL_GROUP_CHANGE_REASON_NONE)
 
     def AddMembers(self, contacts, message):
-        for handle_id in contacts:
-            handle = self._conn_ref().handle(telepathy.HANDLE_TYPE_CONTACT, handle_id)
-            logger.info('Inviting new contact, %s, to chat' % handle.account)
-            self._conversation.invite_user(handle.contact)
+        raise telepathy.PermissionDenied("We can't add members")
 
     @async
     def __add_initial_participants(self):
         handles = []
         handles.append(self._conn.self_handle)
-        if self._conversation:
-            for participant in self._conversation.participants:
-                handle = self._conn.ensure_contact_handle(contact)
+        if self._room:
+            for user in self._room.users:
+                name = user['username']
+                # FIXME: This sender is a hack to make a room specific handle
+                handle = self._conn.ensureContactHandle(name, sender=self._object_path)
                 handles.append(handle)
 
         if handles:
